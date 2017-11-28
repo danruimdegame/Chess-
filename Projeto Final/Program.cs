a83923adc28dba2c044b71a255d9693b7ebf87b8
danruimdegame <danieltomaz1931@gmail.com>
2017-11-28 21:39:28 -0200

    Projeto STAGE 2 - Classe Posição

--- Projeto Final/Program.cs
+++ Projeto Final/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using CAMPO_DE_BATALHA;
 namespace Projeto_Final
 {
     class Program
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Console.WriteLine("Olá");
-                Console.ReadLine();
-            }
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+         
     }
 }
