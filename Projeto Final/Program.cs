78e5d714489837dfe5d354d97b86266380dabd59
Danzinho do Quero <danieltomaz1931@gmail.com>
2017-11-29 09:42:43 -0200

    Projeto STAGE 7 - Criada a classe PosicaoXadrez

--- Projeto Final/Program.cs
+++ Projeto Final/Program.cs
@@ -8,20 +8,11 @@ namespace Projeto_Final
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-                tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Azul), new Posicao(2, 4));
+            Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
