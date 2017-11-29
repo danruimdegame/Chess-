6ca4ef016f041b2a698131482bc4dbd2325ae780
danruimdegame <danieltomaz1931@gmail.com>
2017-11-28 22:01:43 -0200

    Projeto STAGE 3 - Implementando o Tabuleiro

---
+++ Projeto Final/CAMPO DE BATALHA/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMPO_DE_BATALHA
+{
+    enum Cor
+    {
+        Azul,
+        Vermelha,
+        Branca,
+        Preta
+    }
+}
--- Projeto Final/Program.cs
+++ Projeto Final/Program.cs
@@ -6,11 +6,7 @@ namespace Projeto_Final
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
